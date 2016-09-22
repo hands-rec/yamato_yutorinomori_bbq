191d3ddcabe0ea534dea5aae867ba4b0f71beea7
hands-rec <hands.records@gmail.com>
2016-09-22 09:43:37 +0900

    Enable to work

--- lib/yamato_yutorinomori_bbq.rb
+++ lib/yamato_yutorinomori_bbq.rb
@@ -1,5 +1,88 @@
 require "yamato_yutorinomori_bbq/version"
 
+require 'nokogiri'
+require 'date'
+require 'open-uri'
+require 'openssl'
+
 module YamatoYutorinomoriBbq
-  # Your code goes here...
+
+  class Client
+    CRAWLER_UA = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; Trident/7.0; rv:11.0) like Gecko'
+    def initialize()
+      @site_url = "https://175.184.45.152:24135/yamato/rv/index.php"
+    end
+
+    def find_bookable_list
+      source = open(@site_url, 'User-Agent' => CRAWLER_UA, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE)
+      scraper = YamatoYutorinomoriBbq::Scraper.new(source)
+      scraper.find_bookable_list
+    end
+  end
+
+  class Scraper
+    attr_reader :base
+    def initialize(source)
+      @base = Nokogiri::HTML(source).css('#undercolumn_entry table')
+      @date_time_list = []
+    end
+
+    def execute
+      @base.css('tr').each do |tr|
+        date = scrape_date(tr)
+        next if date.empty?
+
+        values = scrape_values(tr)
+
+        values.each do |time_id, value|
+          @date_time_list << BookableDateTime.new(date, time_id, value) 
+        end
+      end
+      @date_time_list
+    end
+
+    def find_bookable_list
+      execute if @date_time_list.length == 0
+      list = []
+      @date_time_list.each do |datetime|
+        list << datetime if datetime.available?
+      end
+      list
+    end
+
+    def scrape_date(tr)
+      tr.css('td:first-child').text.strip
+    end
+
+    def scrape_values(tr)
+      first_half = tr.css('td:nth-child(2)').text.strip
+      second_half = tr.css('td:nth-child(9)').text.strip
+      {1 => first_half, 2 => second_half}
+    end
+
+  end
+
+  class BookableDateTime
+    def initialize(date, time_id, value)
+      @date = date
+      @time_id = time_id
+      @value = value
+    end
+
+    def available?
+      @value != "×"
+    end
+
+    def to_s
+      "#{date} #{time}"
+    end
+
+    def time
+      if @time_id == 1
+        "10時 ～"
+      elsif @time_id == 2
+        "13時30分 ～"
+      end
+    end
+  end
 end
--- spec/yamato_yutorinomori_bbq_spec.rb
+++ spec/yamato_yutorinomori_bbq_spec.rb
@@ -1,11 +1,74 @@
 require 'spec_helper'
+require 'pp'
 
 describe YamatoYutorinomoriBbq do
   it 'has a version number' do
     expect(YamatoYutorinomoriBbq::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+
+  describe "#find_bookable_list" do
+#    pp YamatoYutorinomoriBbq::Client.new().find_bookable_list
+  end
+end
+
+describe YamatoYutorinomoriBbq::Scraper do
+  before :each do
+    doc = open('./spec/data/scraping/bbq.html')
+    @scraper = YamatoYutorinomoriBbq::Scraper.new(doc)
+  end
+
+  describe "#scrape_date" do
+    it 'empty' do
+      tr = @scraper.base.css('tr:first-child')
+      expect(@scraper.scrape_date(tr)).to be_empty
+    end
+
+    it 'first' do
+      tr = @scraper.base.css('tr:nth-child(2)')
+      expect(@scraper.scrape_date(tr)).to eq "09/22(木)" 
+    end
+  end
+
+  describe "#scrape_values" do
+    it '◎' do
+      tr = @scraper.base.css('tr:nth-child(3)')
+      values = @scraper.scrape_values(tr)
+      expect(values[1]).to eq "◎"
+      expect(values[2]).to eq "◎"
+    end
+
+    it '◯' do
+      tr = @scraper.base.css('tr:nth-child(20)')
+      values = @scraper.scrape_values(tr)
+      expect(values[1]).to eq "◯"
+      expect(values[2]).to eq "◯"
+    end
+
+    it '△' do
+      tr = @scraper.base.css('tr:nth-child(4)')
+      values = @scraper.scrape_values(tr)
+      expect(values[1]).to eq "△"
+      expect(values[2]).to eq "△"
+    end
+    
+    it '×' do
+      tr = @scraper.base.css('tr:nth-child(2)')
+      values = @scraper.scrape_values(tr)
+      expect(values[1]).to eq "×"
+      expect(values[2]).to eq "×"
+    end
+  end
+
+  describe '#execute' do
+    it 'list count 60' do
+      expect(@scraper.execute.length).to eq 60
+    end
+  end
+
+  describe '#find_bookable_list' do
+    it 'list count 41' do
+      expect(@scraper.find_bookable_list.length).to eq 41 
+    end
   end
 end
